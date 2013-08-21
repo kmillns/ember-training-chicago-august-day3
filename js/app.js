33c670981d1de7960e2be9e0f628cf2b644125f5
Kyle Millns <kyle.millns@gmail.com>
2013-08-21 15:40:34 -0500

    Copying Day 2 work over to Day 3

--- js/app.js
+++ js/app.js
@@ -1,91 +1,148 @@
+// app.js
 (function() {
-"use strict";
+  'use strict';
 
-window.App = Ember.Application.create();
-
-App.Router.map(function() {
-  this.resource('album', { path: '/album/:album_id' });
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return App.ALBUM_FIXTURES;
-  }
-});
-
-App.AlbumRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
-  },
-
-  events: {
-    play: function(song) {
-      this.controllerFor('nowPlaying').set('model', song);
-    }
-  }
-});
-
-App.NowPlayingController = Ember.ObjectController.extend();
-
-App.Song = Ember.Object.extend();
-App.Album = Ember.Object.extend({
-  totalDuration: function() {
-    var sum = 0;
-
-    var foo = "bar";
-    this.get('songs').forEach(function(song) {
-      eval('');
-      sum += song.get('duration');
-    });
-
-    return sum;
-  }.property('songs.@each.duration')
-});
-
-App.AudioPlayerComponent = Ember.Component.extend({
-  classNames: 'audio-control',
-
-  duration: 0,
-  currentTime: 0,
-  isLoaded: false,
-
-  didInsertElement: function() {
-    var $audio = this.$('audio'),
-        component = this;
-
-    $audio.on('loadedmetadata', function() {
-      component.set('duration', Math.floor(this.duration));
-      component.set('isLoaded', true);
-    });
-
-    $audio.on('timeupdate', function() {
-      component.set('currentTime', Math.floor(this.currentTime));
-    });
-
-    $audio.on('play', function() {
-      component.set('isPlaying', true);
-    });
-
-    $audio.on('pause', function() {
-      component.set('isPlaying', false);
-    });
-  }
-});
-
-Ember.Handlebars.helper('format-duration', function(seconds) {
-  var minutes = Math.floor(seconds/60);
-  var remainingSeconds = seconds % 60;
-
-  var result = '';
-  if (remainingSeconds < 10) {
-    result = "0";
-  }
-
-  result += String(remainingSeconds);
-
-  result = minutes + ":" + result;
-
-  return result;
-});
-
+  window.App = Ember.Application.create();
 })();
+
+// router.js
+(function (App) {
+  'use strict';
+
+  App.Router.map(function () {
+    this.resource('album', {
+      path: '/album/:album_id'
+    });
+  });
+})(window.App);
+
+// routes/Index.js
+(function (App) {
+  'use strict';
+
+  App.IndexRoute = Ember.Route.extend({
+    model: function () {
+      return App.ALBUM_FIXTURES;
+    }
+  });
+})(window.App);
+
+// routes/Album.js
+(function (App) {
+  'use strict';
+
+  App.AlbumRoute = Ember.Route.extend({
+    model: function (params) {
+      return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
+    },
+    events: {
+      play: function (song) {
+        this.controllerFor('nowPlaying').set('model', song);
+      }
+    }
+  });
+})(window.App);
+
+// models/Album.js
+(function (App) {
+  'use strict';
+
+  App.Album = Ember.Object.extend({
+    totalDuration: function () {
+      return this.songs.reduce(function (totalDuration, song) {
+        return totalDuration + song.duration;
+      }, 0);
+    }.property('songs.@each.duration')
+  });
+})(window.App);
+
+// models/Song.js
+(function (App) {
+  'use strict';
+
+  App.Song = Ember.Object.extend({
+    
+  });
+})(window.App);
+
+// controllers/NowPlaying.js
+(function (App) {
+  'use strict';
+
+  App.NowPlayingController = Ember.ObjectController.extend({
+
+  });
+})(window.App);
+
+// helpers/format-duration.js
+(function () {
+  'use strict';
+
+  Ember.Handlebars.helper('format-duration', function(value, options) {
+    var seconds = parseInt(value, 10);
+    var minutes = Math.floor(seconds/60);
+
+    seconds = seconds - (minutes * 60);
+
+    var prettyTime = minutes + ':';
+
+    if (seconds < 10) {
+      prettyTime = prettyTime + '0';
+    }
+
+    prettyTime = prettyTime + seconds;
+
+    return prettyTime;
+  });
+})();
+
+// components/AudioPlayer.js
+(function (App) {
+  App.AudioPlayerComponent = Ember.Component.extend({
+    classNames: ['audio-control'],
+    currentTime: 0,
+    duration: null,
+    isLoaded: false,
+    isPlaying: false,
+    play: function () {
+      this.$('audio')[0].play();
+    },
+    pause: function () {
+      this.$('audio')[0].pause();
+    },
+    didInsertElement: function () {
+      var component = this;
+
+      component.$('audio')
+        .on('timeupdate', function () {
+          component.set('currentTime', Math.floor(this.currentTime));
+        })
+        .on('loadeddata', function () {
+          component.set('duration', Math.floor(this.duration));
+          component.set('isLoaded', true);
+        })
+        .on('play', function () {
+          component.set('isPlaying', true);
+        })
+        .on('pause', function () {
+          component.set('isPlaying', false);
+        });
+    }
+  });
+})(window.App);
+
+// components/ToggleTime.js
+(function (App) {
+  App.ToggleTimeComponent = Ember.Component.extend({
+    showingCurrent: true,
+    toggleTime: function () {
+      this.set('showingCurrent', !this.get('showingCurrent'));
+    },
+    remainingTime: function () {
+      var currentTime = this.get('currentTime');
+      var totalTime = this.get('duration');
+
+      return totalTime - currentTime;
+    }.property('currentTime', 'duration')
+  });
+})(window.App);
--- tests/step16.js
+++ tests/step16.js
@@ -3,6 +3,9 @@
   currently playing song. If you click on it, it should hide the total duration
   and show the time remaining.
 
+  Remember, if you want to reference the duration in your template, you should
+  use `view.duration`.
+
   By now, this should be a pretty simple feature for you to implement. Just use
   the concepts you put to use yesterday.
 
@@ -16,12 +19,80 @@
   * When you play a song, the current time should appear in the Now Playing panel.
   * When you click on the current time, it should show the remaining time.
 
-  Note: You'll probably need to use jQuery to set up a listener to know when
-  the song has loaded. Because these tests are asynchronous, make sure you use
-  QUnit's start() and stop() methods. If you finish this early, you should
-  write a helper to clean up this code.
+  Note: You'll probably need to use jQuery to set up a listener to know when the
+  song has loaded. Because these tests are asynchronous, make sure you use QUnit's
+  start() and stop() methods. If you finish this early, you should write a helper
+  to clean up this code.
 
   Good luck!
 */
 
 step(16, "Click to Toggle Remaining Time");
+
+// Unit tests of toggleTime in isolation
+testComponent('toggleTime', 'remainingTime should be total minus current', function (component) {
+  Ember.run(function() {
+    component.set('duration', 10);
+    component.set('currentTime', 3)
+  });
+
+  strictEqual(component.get('remainingTime'), 7);
+});
+
+testComponent('toggleTime', 'with showingCurrent true, currentTime should be displayed', function (component) {
+  Ember.run(function() {
+    component.set('showingCurrent', true);
+  });
+
+  strictEqual(component.$('.currentTime').length, 1);
+});
+
+testComponent('toggleTime', 'with showingCurrent false, remainingTime should be displayed', function (component) {
+  Ember.run(function() {
+    component.set('showingCurrent', false);
+  });
+
+  strictEqual(component.$('.remainingTime').length, 1);
+});
+
+testComponent('toggleTime', 'calling toggleTime should swap showingCurrent', function (component) {
+  Ember.run(function() {
+    component.set('showingCurrent', false);
+
+    component.toggleTime();
+    ok(component.get('showingCurrent'), "started as false, should be true after one toggle");
+
+
+    component.toggleTime();
+    ok(!component.get('showingCurrent'), "started as false, should be false again after two toggles");
+  });
+});
+
+// Integration tests inside audioPlayer
+testComponent('audioPlayer', 'If no song is playing, the current time should not appear.', function (component) {
+  strictEqual(component.$('.currentTime').length, 0);
+});
+
+testComponent('audioPlayer', 'When you play a song, the current time should appear in the Now Playing panel.', function (component) {
+  Ember.run(function() {
+    component.set('src', "audio/Southern_Nights_-_07_-_All_My_Sorrows.mp3");
+  });
+
+  waitFor(component, 'isPlaying').then(function() {
+    strictEqual(component.$('.currentTime').length, 1);
+    strictEqual(component.$('.remainingTime').length, 0);
+  });
+});
+
+testComponent('audioPlayer', 'When you click on the current time, it should show the remaining time.', function (component) {
+  Ember.run(function() {
+    component.set('src', "audio/Southern_Nights_-_07_-_All_My_Sorrows.mp3");
+  });
+
+  waitFor(component, 'isPlaying').then(function() {
+    click('.currentTime', component);
+  }).then(function() {
+    strictEqual(component.$('.currentTime').length, 0);
+    strictEqual(component.$('.remainingTime').length, 1);
+  });
+});
